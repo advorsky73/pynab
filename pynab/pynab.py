78e633fee45b085b000ea014d8f8b2f14e4089bf
ADvorsky <alexander.dvorsky@chello.at>
2018-04-23 21:28:07 +0200

    added a lot of unittest code; refactored methods in pynab.py; all pylint checked now without issues

--- pynab/pynab.py
+++ pynab/pynab.py
@@ -4,11 +4,11 @@
 This module provides classes for easy handling of the YNAB API.
 """
 
-import sys
+from urllib.parse import urlencode
+from collections import namedtuple
+
 import json
 import requests
-import urllib
-from collections import namedtuple
 
 
 class YNABSession(object):
@@ -23,11 +23,9 @@ class YNABSession(object):
         # create the header with the Bearer token for YNAB
         self.requests_header = {"accept": "application/json",
                                 "Authorization": "Bearer " + ynab_access_token}
-
         # create the requests session with the custom header fields
         self.session = requests.Session()
         self.session.headers.update(self.requests_header)
-
         # base url for all api calls
         self.base_url = "https://api.youneedabudget.com/v1/"
 
@@ -43,7 +41,7 @@ class YNABSession(object):
     def _build_json_object(json_string):
         """
         creates an object with attributes from json attributes
-        :param json_string:
+        :param json_string: json string representation
         :return: python object
         """
         return json.loads(json_string, object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
@@ -59,26 +57,31 @@ class YNABSession(object):
                json_data["error"]["name"] + " (" + \
                json_data["error"]["detail"] + ")"
 
-    def _internal_get_stuff(self, url, key1, key2, check_for_404=False):
+    def _internal_get_stuff(self, url, key1, key2, key2alt=None):
         """
         get information from YNAB the generic way
+        :param url: url part for the request appended to base_url member
+        :param key1: first key to access json dictionary after retrieval
+        :param key2: second key to access json dictionary after retrieval
+        :param key2alt: alternative second key to access json dictionary after retrieval
         :return: (list of) object(s) with information about requested data
+                 if key2alt is set, then a second object is returned representing it
         :throws: if an error occurs an exception is raised
         """
         # get the response from YNAB
-        r = self.session.get(self.base_url + url)
-
+        result = self.session.get(self.base_url + url)
         # check for success
-        if r.status_code == 200:
-            result = json.dumps(json.loads(r.text)[key1][key2])
-            return self._build_json_object(result)
-
+        if result.status_code == 200:
+            resultkey = json.dumps(json.loads(result.text)[key1][key2])
+            if key2alt is None:
+                return self._build_json_object(resultkey)
+            resultkey2 = json.dumps(json.loads(result.text)[key1][key2alt])
+            return self._build_json_object(resultkey), self._build_json_object(resultkey2)
         # check for an empty account
-        if check_for_404 and r.status_code == 404:
+        if result.status_code == 404:
             return None
-
         # build error information and raise an exception
-        raise Exception(self._build_exception_string(json.loads(r.text)))
+        raise Exception(self._build_exception_string(json.loads(result.text)))
 
     def get_user(self):
         """
@@ -86,152 +89,166 @@ class YNABSession(object):
         :return: object with user information
         :throws: if an error occurs an exception is raised
         """
-
         # get the response from YNAB
-        r = self.session.get(self.base_url + "user")
-
+        result = self.session.get(self.base_url + "user")
         # check for success
-        if r.status_code == 200:
-            user = json.dumps(json.loads(r.text)["data"]["user"])
+        if result.status_code == 200:
+            user = json.dumps(json.loads(result.text)["data"]["user"])
             return self._build_json_object(user)
-
         # build error information and raise an exception
-        raise Exception(self._build_exception_string(json.loads(r.text)))
+        raise Exception(self._build_exception_string(json.loads(result.text)))
 
     def get_budgets(self, budget_id=None, last_knowledge_of_server=None):
         """
         get budget(s) information from YNAB
+        :param budget_id: optional; id of the budget to be received. If not set all budgets
+                will be retrieved
+        :param last_knowledge_of_server: optional; The starting server knowledge. If provided,
+                only entities that have changed since last_knowledge_of_server will be included.
         :return: (list of) object(s) with information about budget(s)
+                 if budget_id is presented a second return value stands for server_knowledge
         :throws: if an error occurs an exception is raised
         """
-        url = "budgets"
         # get the response from YNAB
+        url = "budgets"
         if budget_id is None:
-            return self._internal_get_stuff(url, 'data', 'budgets', True)
-        else:
-            if last_knowledge_of_server is None:
-                return self._internal_get_stuff(url + "/" + budget_id, 'data', 'budget', True)
-            else:
-                url = url + "/" + budget_id + \
-                      "?last_knowledge_of_server =" + last_knowledge_of_server
-                return self._internal_get_stuff(url, 'data', 'budget', True)
+            return self._internal_get_stuff(url, 'data', 'budgets')
+        if last_knowledge_of_server is None:
+            return self._internal_get_stuff(url + "/" + budget_id,
+                                            'data',
+                                            'budget',
+                                            'server_knowledge')
+        return self._internal_get_stuff(url, 'data', 'budget')
 
     def get_accounts(self, budget_id, account_id=None):
         """
         get account(s) information from YNAB
+        :param budget_id:  id of the budget to get the account data from
+        :param account_id: optional; id of the account to be received. If not set all accounts will
+                be retrieved
         :return: (list of) object(s) with information about account(s)
         :throws: if an error occurs an exception is raised
         """
-        url = "budgets/" + budget_id + "/accounts"
         # get the response from YNAB
+        url = "budgets/" + budget_id + "/accounts"
         if account_id is None:
-            return self._internal_get_stuff(url, 'data', 'accounts', True)
-        else:
-            return self._internal_get_stuff(url + "/" + account_id, 'data', 'account', True)
+            return self._internal_get_stuff(url, 'data', 'accounts')
+        return self._internal_get_stuff(url + "/" + account_id, 'data', 'account')
 
     def get_categories(self, budget_id, category_id=None):
         """
         get categorie(s) information from YNAB
+        :param budget_id:  id of the budget to get the account data from
+        :param category_id: optional; id of the category to be received. If not set all categories
+                will be retrieved
         :return: (list of) object(s) with information about categorie(s)
         :throws: if an error occurs an exception is raised
         """
-        url = "budgets/" + budget_id + "/categories"
         # get the response from YNAB
+        url = "budgets/" + budget_id + "/categories"
         if category_id is None:
-            return self._internal_get_stuff(url, 'data', 'category_groups', True)
-        else:
-            return self._internal_get_stuff(url + "/" + category_id, 'data', 'category', True)
+            return self._internal_get_stuff(url, 'data', 'category_groups')
+        return self._internal_get_stuff(url + "/" + category_id, 'data', 'category')
 
     def get_payees(self, budget_id, payee_id=None):
         """
         get payee(s) information from YNAB
+        :param budget_id:  id of the budget to get the account data from
+        :param payee_id: optional; id of the payee to be received. If not set all payees will be
+                retrieved
         :return: (list of) object(s) with information about payee(s)
         :throws: if an error occurs an exception is raised
         """
-        url = "budgets/" + budget_id + "/payees"
         # get the response from YNAB
+        url = "budgets/" + budget_id + "/payees"
         if payee_id is None:
-            return self._internal_get_stuff(url, 'data', 'payees', True)
-        else:
-            return self._internal_get_stuff(url + "/" + payee_id, 'data', 'payee', True)
+            return self._internal_get_stuff(url, 'data', 'payees')
+        return self._internal_get_stuff(url + "/" + payee_id, 'data', 'payee')
 
     def get_months(self, budget_id, month_id=None):
         """
         get month(s) information from YNAB
+        :param budget_id:  id of the budget to get the account data from
+        :param month_id: optional; id of the month to be received. If not set all months will be
+                retrieved
         :return: (list of) object(s) with information about month(s)
         :throws: if an error occurs an exception is raised
         """
-        url = "budgets/" + budget_id + "/months"
         # get the response from YNAB
+        url = "budgets/" + budget_id + "/months"
         if month_id is None:
-            return self._internal_get_stuff(url, 'data', 'months', True)
-        else:
-            return self._internal_get_stuff(url + "/" + month_id, 'data', 'month', True)
+            return self._internal_get_stuff(url, 'data', 'months')
+        return self._internal_get_stuff(url + "/" + month_id, 'data', 'month')
 
-    def get_transactions(self,
-                         budget_id,
-                         account_id=None,
-                         categories_id=None,
-                         payees_id=None,
-                         transaction_id=None,
-                         since_date=None,
-                         ttype=None):
+    def get_transactions(self, budget_id, transaction_id=None, since_date=None, ttype=None):
         """
         get transaction(s) information from YNAB
+        :param budget_id:       all transactions for this budget will be retrieved if set alone
+        :param transaction_id:  optional; only one specific transaction will be retrieved
+        :param since_date:      optional; limit the retrieved data to transactions since this date
+        :param ttype:           optional; limit the retrieved data to transactions matching the type
         :return: (list of) object(s) with information about transaction(s)
         :throws: if an error occurs an exception is raised
         """
-        url = "budgets/" + budget_id + "/transactions"
         # get the response from YNAB
+        url = "budgets/" + budget_id + "/transactions"
+        url_vars = {}
         if transaction_id is None:
-            if account_id is not None:
-                assert(categories_id is None and payees_id is None)
-                url = "budgets/" + budget_id + "/accounts/" + account_id + "/transactions"
-            if categories_id is not None:
-                assert(account_id is None and payees_id is None)
-                url = "budgets/" + budget_id + "/categories/" + categories_id + "/transactions"
-            if payees_id is not None:
-                assert(account_id is None and categories_id is None)
-                url = "budgets/" + budget_id + "/payees/" + payees_id + "/transactions"
-            url_vars = {}
             if since_date is not None:
                 url_vars.update({'since_date': since_date})
             if ttype is not None:
-                assert(account_id is None and
-                       categories_id is None and
-                       payees_id is None)
                 url_vars.update({'type': ttype})
-            return self._internal_get_stuff(url + urllib.parse.urlencode(url_vars),
-                                            'data', 'transactions', True)
-        else:
-            assert(since_date is None and
-                   ttype is None and
-                   account_id is None and
-                   payees_id is None and
-                   categories_id is None)
-            return self._internal_get_stuff(url + "/" + transaction_id, 'data', 'transaction', True)
+            return self._internal_get_stuff(url + urlencode(url_vars), 'data', 'transactions')
+        return self._internal_get_stuff(url + "/" + transaction_id, 'data', 'transaction')
+
+    def get_transactions_for_account(self, budget_id, account_id, since_date=None):
+        """
+        get transaction(s) information from YNAB
+        :param budget_id:  all transactions for this budget will be retrieved if set alone
+        :param account_id: all transactions for this account will be retrieved
+        :param since_date: optional; limit the retrieved data to transactions since this date
+        :return: (list of) object(s) with information about transaction(s)
+        :throws: if an error occurs an exception is raised
+        """
+        # get the response from YNAB
+        url_vars = {}
+        url = "budgets/" + budget_id + "/accounts/" + account_id + "/transactions"
+        if since_date is not None:
+            url_vars.update({'since_date': since_date})
+        return self._internal_get_stuff(url + urlencode(url_vars), 'data', 'transactions')
+
+    def get_transactions_for_category(self, budget_id, category_id, since_date=None):
+        """
+        get transaction(s) information from YNAB
+        :param budget_id:   all transactions for this budget will be retrieved if set alone
+        :param category_id: all transactions for this category will be retrieved
+        :param since_date:  optional; limit the retrieved data to transactions since this date
+        :return: (list of) object(s) with information about transaction(s)
+        :throws: if an error occurs an exception is raised
+        """
+        # get the response from YNAB
+        url_vars = {}
+        url = "budgets/" + budget_id + "/categories/" + category_id + "/transactions"
+        if since_date is not None:
+            url_vars.update({'since_date': since_date})
+        return self._internal_get_stuff(url + urlencode(url_vars), 'data', 'transactions')
+
+    def get_transactions_for_payee(self, budget_id, payees_id, since_date=None):
+        """
+        get transaction(s) information from YNAB
+        :param budget_id:  all transactions for this budget will be retrieved if set alone
+        :param payees_id:  all transactions for this payee will be retrieved
+        :param since_date: optional; limit the retrieved data to transactions since this date
+        :return: (list of) object(s) with information about transaction(s)
+        :throws: if an error occurs an exception is raised
+        """
+        # get the response from YNAB
+        url_vars = {}
+        url = "budgets/" + budget_id + "/payees/" + payees_id + "/transactions"
+        if since_date is not None:
+            url_vars.update({'since_date': since_date})
+        return self._internal_get_stuff(url + urlencode(url_vars), 'data', 'transactions')
 
 
 if __name__ == '__main__':
-    #y = YNABSession(sys.argv[1])
-    #b = y.get_budgets()
-    #print(b)
-    #bt = y.get_budgets(b[0].id)
-    #print(bt)
-    #a = y.get_accounts(b[0].id)
-    #for aa in a:
-    #    print(aa)
-    #c = y.get_categories(b[0].id)
-    #for cc in c:
-    #    print(cc)
-    #p = y.get_payees(b[0].id)
-    #for pp in p:
-    #    print(pp)
-    #m = y.get_months(b[0].id)
-    #for mm in m:
-    #    print(mm)
-    #t = y.get_transactions(b[0].id)
-    #for tt in t:
-    #    print(tt)
     print("Module not ment to run on its own...")
-
--- test/test_pynab.py
+++ test/test_pynab.py
@@ -12,6 +12,7 @@ class TestYNABModule(unittest.TestCase):
     """
     Test class for pynab.py
     """
+
     def setUp(self):
         self.api_token = ""
         self.ynab_session = YNABSession(self.api_token)
@@ -19,23 +20,225 @@ class TestYNABModule(unittest.TestCase):
     def tearDown(self):
         del self.ynab_session
 
+    def _verify_budget(self, budget):
+        """
+        Verifies the budget json node and its children
+        :param budget: object representing a budget
+        :return: nothing
+        """
+        self.assertIsNotNone(budget)
+        self.assertIsNotNone(budget.id)
+        self.assertIsNotNone(budget.name)
+        if hasattr(budget, 'last_modified_on'):
+            self.assertIsNotNone(budget.last_modified_on)
+        self.assertIsNotNone(budget.date_format)
+        self.assertIsNotNone(budget.date_format.format)
+        self.assertIsNotNone(budget.currency_format)
+        self.assertIsNotNone(budget.currency_format.iso_code)
+        self.assertIsNotNone(budget.currency_format.example_format)
+        self.assertIsNotNone(budget.currency_format.decimal_digits)
+        self.assertIsNotNone(budget.currency_format.decimal_separator)
+        self.assertIsNotNone(budget.currency_format.symbol_first)
+        self.assertIsNotNone(budget.currency_format.group_separator)
+        self.assertIsNotNone(budget.currency_format.currency_symbol)
+        self.assertIsNotNone(budget.currency_format.display_symbol)
+
+    def _verify_account(self, account):
+        """
+        Verifies the account json node and its children
+        :param budget: object representing an account
+        :return: nothing
+        """
+        self.assertIsNotNone(account)
+        self.assertIsNotNone(account.id)
+        self.assertIsNotNone(account.name)
+        self.assertIsNotNone(account.type)
+        self.assertIsNotNone(account.on_budget)
+        self.assertIsNotNone(account.closed)
+        self.assertTrue(hasattr(account, 'note'))
+        self.assertIsNotNone(account.balance)
+        self.assertIsNotNone(account.cleared_balance)
+        self.assertIsNotNone(account.uncleared_balance)
+
+    def _verify_payee(self, payee):
+        """
+        Verifies the payee json node and its children
+        :param budget: object representing a payee
+        :return: nothing
+        """
+        self.assertIsNotNone(payee)
+        self.assertIsNotNone(payee.id)
+        self.assertIsNotNone(payee.name)
+        self.assertTrue(hasattr(payee, 'transfer_account_id'))
+
+    def _verify_payee_location(self, payee_location):
+        """
+        Verifies the payee_location json node and its children
+        :param budget: object representing a payee_location
+        :return: nothing
+        """
+        self.assertIsNotNone(payee_location)
+        self.assertIsNotNone(payee_location.id)
+        self.assertIsNotNone(payee_location.payee_id)
+        self.assertTrue(hasattr(payee_location, 'latitude'))
+        self.assertTrue(hasattr(payee_location, 'longitude'))
+
+    def _verify_category_group(self, category_group):
+        """
+        Verifies the category_group json node and its children
+        :param budget: object representing a category_group
+        :return: nothing
+        """
+        self.assertIsNotNone(category_group)
+        self.assertIsNotNone(category_group.id)
+        self.assertIsNotNone(category_group.name)
+        self.assertIsNotNone(category_group.hidden)
+
+    def _verify_category(self, category):
+        """
+        Verifies the category json node and its children
+        :param budget: object representing a category
+        :return: nothing
+        """
+        self.assertIsNotNone(category)
+        self.assertIsNotNone(category.id)
+        self.assertIsNotNone(category.category_group_id)
+        self.assertIsNotNone(category.name)
+        self.assertIsNotNone(category.hidden)
+        self.assertIsNotNone(category.name)
+        self.assertTrue(hasattr(category, 'note'))
+        self.assertIsNotNone(category.budgeted)
+        self.assertIsNotNone(category.activity)
+        self.assertIsNotNone(category.balance)
+
+    def _verify_month(self, month):
+        """
+        Verifies the month json node and its children
+        :param budget: object representing a month
+        :return: nothing
+        """
+        self.assertIsNotNone(month)
+        self.assertIsNotNone(month.month)
+        self.assertTrue(hasattr(month, 'note'))
+        self.assertIsNotNone(month.to_be_budgeted)
+        self.assertIsNotNone(month.age_of_money)
+
+    def _verify_transaction(self, transaction):
+        """
+        Verifies the transaction json node and its children
+        :param budget: object representing a transaction
+        :return: nothing
+        """
+        self.assertIsNotNone(transaction)
+        self.assertIsNotNone(transaction.id)
+        self.assertIsNotNone(transaction.date)
+        self.assertIsNotNone(transaction.amount)
+        self.assertTrue(hasattr(transaction, 'memo'))
+        self.assertIsNotNone(transaction.cleared)
+        self.assertIsNotNone(transaction.approved)
+        self.assertTrue(hasattr(transaction, 'flag_color'))
+        self.assertIsNotNone(transaction.account_id)
+        self.assertTrue(hasattr(transaction, 'payee_id'))
+        self.assertTrue(hasattr(transaction, 'category_id'))
+        self.assertTrue(hasattr(transaction, 'transfer_account_id'))
+        self.assertTrue(hasattr(transaction, 'import_id'))
+
+    def _verify_subtransactions(self, subtransaction):
+        """
+        Verifies the subtransactions json node and its children
+        :param budget: object representing a subtransactions
+        :return: nothing
+        """
+        self.assertIsNotNone(subtransaction)
+        self.assertIsNotNone(subtransaction.transaction_id)
+        self.assertIsNotNone(subtransaction.amount)
+        self.assertTrue(hasattr(subtransaction, 'memo'))
+        self.assertTrue(hasattr(subtransaction, 'payee_id'))
+        self.assertTrue(hasattr(subtransaction, 'category_id'))
+        self.assertTrue(hasattr(subtransaction, 'transfer_account_id'))
+
+    def _verify_scheduled_transactions(self, scheduled_transaction):
+        """
+        Verifies the scheduled_transactions json node and its children
+        :param budget: object representing a scheduled_transactions
+        :return: nothing
+        """
+        self.assertIsNotNone(scheduled_transaction)
+        self.assertIsNotNone(scheduled_transaction.id)
+        self.assertIsNotNone(scheduled_transaction.date_first)
+        self.assertIsNotNone(scheduled_transaction.date_next)
+        self.assertIsNotNone(scheduled_transaction.frequency)
+        self.assertIsNotNone(scheduled_transaction.amount)
+        self.assertTrue(hasattr(scheduled_transaction, 'memo'))
+        self.assertTrue(hasattr(scheduled_transaction, 'flag_color'))
+        self.assertIsNotNone(scheduled_transaction.account_id)
+        self.assertTrue(hasattr(scheduled_transaction, 'payee_id'))
+        self.assertTrue(hasattr(scheduled_transaction, 'category_id'))
+        self.assertTrue(hasattr(scheduled_transaction, 'transfer_account_id'))
+
+    def _verify_scheduled_subtransactions(self, scheduled_subtransaction):
+        """
+        Verifies the scheduled_subtransactions json node and its children
+        :param budget: object representing a scheduled_subtransactions
+        :return: nothing
+        """
+        self.assertIsNotNone(scheduled_subtransaction)
+        self.assertIsNotNone(scheduled_subtransaction.scheduled_transaction_id)
+        self.assertIsNotNone(scheduled_subtransaction.amount)
+        self.assertTrue(hasattr(scheduled_subtransaction, 'memo'))
+        self.assertTrue(hasattr(scheduled_subtransaction, 'payee_id'))
+        self.assertTrue(hasattr(scheduled_subtransaction, 'category_id'))
+        self.assertTrue(hasattr(scheduled_subtransaction, 'transfer_account_id'))
+
     def test_get_user(self):
-        u = self.ynab_session.get_user()
-        print(u)
-        print(u.id)
+        """
+        This tests the get_user() from class YNABSession
+        :return: nothing
+        """
+        user = self.ynab_session.get_user()
+        self.assertIsNotNone(user)
+        self.assertIsNotNone(user.id)
 
     def test_get_budgets(self):
-        b = self.ynab_session.get_budgets()
-        for bs in b:
-           print(bs)
+        """
+        This tests the get_budgets() from class YNABSession without parameter budget_id
+        :return: nothing
+        """
+        budgets = self.ynab_session.get_budgets()
+        for budget in budgets:
+            self._verify_budget(budget)
 
-    def test_get_budgets_with_parameter(self):
-        b = self.ynab_session.get_budgets()
-        bb = self.ynab_session.get_budgets(b[0].id)
-        for bs in bb:
-           print(bs)
-
-
+    def test_get_budgets_with_budget_id(self):
+        """
+        This tests the get_budgets() from class YNABSession with parameter budget_id
+        :return: nothing
+        """
+        budgets = self.ynab_session.get_budgets()
+        budget, server_knowledge = self.ynab_session.get_budgets(budgets[0].id)
+        self._verify_budget(budget)
+        for account in budget.accounts:
+            self._verify_account(account)
+        for payee in budget.payees:
+            self._verify_payee(payee)
+        for payee_location in budget.payee_locations:
+            self._verify_payee_location(payee_location)
+        for category_group in budget.category_groups:
+            self._verify_category_group(category_group)
+        for category in budget.categories:
+            self._verify_category(category)
+        for month in budget.months:
+            self._verify_month(month)
+            for category in month.categories:
+                self._verify_category(category)
+        for transaction in budget.transactions:
+            self._verify_transaction(transaction)
+        for subtransaction in budget.subtransactions:
+            self._verify_subtransactions(subtransaction)
+        for scheduled_transaction in budget.scheduled_transactions:
+            self._verify_scheduled_transactions(scheduled_transaction)
+        for scheduled_subtransaction in budget.scheduled_subtransactions:
+            self._verify_scheduled_subtransactions(scheduled_subtransaction)
+        self.assertIsNotNone(server_knowledge)
 
 if __name__ == '__main__':
     unittest.main()
